9363e5a9a4004eafb16c88a753ae24fa6c9e8320
ksruthi1992 <ksruthi1992@gmail.com>
2017-10-18 11:26:29 -0700

    added recipe

--- dashboard/views.py
+++ dashboard/views.py
@@ -11,7 +11,7 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from dashboard.models import Command, SmallTalk
+from dashboard.models import Command, SmallTalk, Recipe
 
 
 class Dashboard(TemplateView):
