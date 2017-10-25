6cc288e0f50280b2eafe831e93826e909ef48def
Swapnil Patel <swapnilpatel357@gmail.com>
2017-10-25 01:49:22 -0700

    static files url to urls.py

--- ava/urls.py
+++ ava/urls.py
@@ -14,9 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import url, include
+from django.conf.urls.static import static
 from django.contrib import admin
+
+from ava import settings
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'', include('dashboard.urls')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
