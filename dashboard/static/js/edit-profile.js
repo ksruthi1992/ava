1c4f47e8a8ad92500056ca87ff2c99dd7924238e
AnandMasurkar <anand.masurkar188@gmail.com>
2017-12-06 07:11:59 -0800

    slider

--- dashboard/static/js/edit-profile.js
+++
@@ -1,6 +0,0 @@
-$(function () {
-   $('#lastname').remove('disabled');
-   console.log($('#lastname').val());
-   $('#lastname').prop('disabled', false);
-      console.log($('#lastname').val());
-    });
